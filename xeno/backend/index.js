79cf795bb2de2640fd5be641ceafaa8a2d9e96bf
Priyanshi Agrawal <priyanshiprofessional1@gmail.com>
2025-05-15 05:43:34 +0530

    fixed login redirect issue

--- xeno/backend/index.js
+++ xeno/backend/index.js
@@ -27,8 +27,8 @@ app.use(session({
     cookie: {
         maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true,
-        secure:true,
-        sameSite: "lax",
+        secure:false,
+        sameSite: "none",
     }
 }));
 
